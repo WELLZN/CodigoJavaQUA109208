8db3a6d32d92f20674ae9e4b57c2f1d351a8d035
WELLZN <wellithuglife@gmail.com>
2023-08-03 17:33:29 -0300

    criando a pasta de projetos

---
+++ atividade/src/principal/atividade01.java
@@ -0,0 +1,21 @@
+package principal;
+
+public class atividade01 {
+
+	public static void main(String[] args) {
+		
+int numero1= 10;
+double numero2= 20;
+char opcao= "s"
+boolean valor = true;
+
+String mensagem = "boa tarde"
+
+sistem.out.println(numero1);
+sistem.out.println(numero2);
+sistem.out.println(opcao);
+sistem.out.println(mensagem);
+sistem.out.println(valor);
+	}
+
+}
