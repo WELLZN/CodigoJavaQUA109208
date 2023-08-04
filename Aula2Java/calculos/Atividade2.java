04ee14ab2f3f7cf2e44ccc54800cf712aecb7be1
WELLZN <wellithuglife@gmail.com>
2023-08-04 17:53:11 -0300

    Aula de Java Calculo

---
+++ Aula2Java/calculos/Atividade2.java
@@ -0,0 +1,21 @@
+package calculos;
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numero1= 10;
+int numero2 = 10;
+int soma = numero1 + numero2;
+int divisão = numero1/ numero2;
+int subtracao = numero1 - numero2;
+int multiplicacao = numero1 * numero2;
+int restoDaDivisao = numero1 % numero2;
+int calculo = (10 + 10) * 10 / 10 ;
+System.out.println("resultado da soma : " + soma );
+System.out.println("Somando 2 numeros: " + (numero1 + numero2));
+System.out.println("Divisão de dois numeros: " + divisao);
+
+	}
+
+}
